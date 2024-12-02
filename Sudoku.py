3fbd46d06a076f8ed218513a72230990d7a5dc55
AidenGeis <aidenmgeis@gmail.com>
2024-12-02 17:59:38 -0500

    Finished Bottom Buttons and Welcome

--- Sudoku.py
+++ Sudoku.py
@@ -12,10 +12,14 @@ def draw_button(screen, rect, text, is_hovered, button_font, button_color, butto
 def main():
     WIDTH = 630
     HEIGHT = 800
+
     pygame.init()
     screen = pygame.display.set_mode((WIDTH, HEIGHT))
     pygame.display.set_caption("Sudoku")
     screen.fill("white")
+
+    welcome_text = 'Welcome to Sudoku!'
+    welcome_font = pygame.font.Font(None, 80)
 
     # Define button properties
     button_font = pygame.font.Font(None, 36)
@@ -30,8 +34,8 @@ def main():
     medium_button = pygame.Rect((WIDTH // 2 - button_width // 2, HEIGHT // 2, button_width, button_height))
     hard_button = pygame.Rect((WIDTH // 2 - button_width // 2, HEIGHT // 2 + 100, button_width, button_height))
     restart_button = pygame.Rect((WIDTH // 2 - game_button_width // 2, HEIGHT - 100, game_button_width, game_button_height))
-    reset_button = pygame.Rect((game_button_width // 2, HEIGHT - 100, game_button_width, game_button_height))
-    exit_button = pygame.Rect((WIDTH/1.666 + game_button_width, HEIGHT - 100, game_button_width, game_button_height))
+    reset_button = pygame.Rect((WIDTH/4 - game_button_width, HEIGHT - 100, game_button_width, game_button_height))
+    exit_button = pygame.Rect((WIDTH*(3/5) + game_button_width, HEIGHT - 100, game_button_width, game_button_height))
 
     board = None
     game_started = False
@@ -43,6 +47,10 @@ def main():
         mouse_click = pygame.mouse.get_pressed()
 
         if not game_started:
+            welcome_surf = welcome_font.render(welcome_text, True, "black")
+            welcome_rect = welcome_surf.get_rect(center=(WIDTH//2, HEIGHT//2 - 200))
+            screen.blit(welcome_surf, welcome_rect)
+
             # Draw buttons
             draw_button(screen, easy_button, "Easy", easy_button.collidepoint(mouse_pos), button_font, button_color, button_hover_color, button_text_color)
             draw_button(screen, medium_button, "Medium", medium_button.collidepoint(mouse_pos), button_font, button_color, button_hover_color, button_text_color)
@@ -72,6 +80,11 @@ def main():
                 else:
                     if restart_button.collidepoint(mouse_pos):
                         main()
+                    if reset_button.collidepoint(mouse_pos):
+                        board.reset_to_original()
+                    if exit_button.collidepoint(mouse_pos):
+                        pygame.quit()
+                        sys.exit()
 
         if board:
             board.draw()
